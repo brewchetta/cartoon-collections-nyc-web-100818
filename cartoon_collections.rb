094c9ec75611c6c51affb8be4a83e75648559c4d
Chett Tiller <chett.tiller@gmail.com>
2018-09-23 11:46:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def roll_call_dwarves(dwarves)
   i = 1
   dwarves.each do |dwarf|
@@ -13,7 +15,8 @@ def long_planeteer_calls(calls)
   calls.include?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+binding.pry
